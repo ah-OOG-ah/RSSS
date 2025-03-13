68f01e3090e41237a7fe688099d2f7e46453d6f2
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-03-12 20:46:25 -0400

    It lives!

--- build.rs
+++ build.rs
@@ -1,8 +1,12 @@
-use spirv_builder::{MetadataPrintout, SpirvBuilder};
+use std::fs::copy;
+use spirv_builder::SpirvBuilder;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    SpirvBuilder::new("rss", "spirv-unknown-vulkan1.0")
-        .print_metadata(MetadataPrintout::Full)
+    println!("Hey! I'm awake!");
+
+    let result = SpirvBuilder::new("./rss", "spirv-unknown-vulkan1.1")
         .build()?;
+    std::fs::create_dir_all("./spirv")?;
+    copy(result.module.unwrap_single(), "./spirv/shader.spv")?;
     Ok(())
 }
